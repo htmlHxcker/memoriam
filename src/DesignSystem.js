2a2895048429f241baa49de2e81d9fc573066e70
htmlHxcker <clintonadeleke@gmail.com>
2021-11-27 14:38:28 +0000

    [feat]: Setup Colors Section

--- src/DesignSystem.js
+++ src/DesignSystem.js
@@ -1,7 +1,155 @@
+import './styles/utilities.css';
+
 const DesignSystem = () => {
 	return (
 		<div className="container bg-soapstone">
 			<h1>Design System</h1>
+
+			<section id="colors" className="text-pickled-bluewood">
+				<h2>
+					<span className="text-jungle-mist">1</span> Colors
+				</h2>
+				<div className="flex">
+					<div>
+						<div
+							style={{
+								padding: '3rem 1rem 1rem',
+								borderRadius: '15px',
+							}}
+							className="bg-squash text-soapstone"
+						>
+							#FDA214
+						</div>
+						<p>
+							<span className="text-jungle-mist">RGB</span> 253, 162, 20
+						</p>
+						<p>
+							<span className="text-jungle-mist">HSL</span> 37, 98%, 54%
+						</p>
+					</div>
+					<div>
+						<div
+							style={{
+								padding: '3rem 1rem 1rem',
+								borderRadius: '15px',
+							}}
+							className="bg-jungle-mist text-firefly"
+						>
+							#BCCED9
+						</div>
+						<p>
+							<span className="text-jungle-mist">RGB</span> 253, 162, 20
+						</p>
+						<p>
+							<span className="text-jungle-mist">HSL</span> 37, 98%, 54%
+						</p>
+					</div>
+					<div>
+						<div
+							style={{
+								padding: '3rem 1rem 1rem',
+								borderRadius: '15px',
+							}}
+							className="bg-pickled-bluewood text-soapstone"
+						>
+							#FDA214
+						</div>
+						<p>
+							<span className="text-jungle-mist">RGB</span> 253, 162, 20
+						</p>
+						<p>
+							<span className="text-jungle-mist">HSL</span> 37, 98%, 54%
+						</p>
+					</div>
+					<div>
+						<div
+							style={{
+								padding: '3rem 1rem 1rem',
+								borderRadius: '15px',
+							}}
+							className="bg-firefly text-soapstone"
+						>
+							#FDA214
+						</div>
+						<p>
+							<span className="text-jungle-mist">RGB</span> 253, 162, 20
+						</p>
+						<p>
+							<span className="text-jungle-mist">HSL</span> 37, 98%, 54%
+						</p>
+					</div>
+				</div>
+				<div className="flex">
+					<div>
+						<div
+							style={{
+								padding: '3rem 1rem 1rem',
+								borderRadius: '15px',
+							}}
+							className="bg-porcelain text-firefly"
+						>
+							#FDA214
+						</div>
+						<p>
+							<span className="text-jungle-mist">RGB</span> 253, 162, 20
+						</p>
+						<p>
+							<span className="text-jungle-mist">HSL</span> 37, 98%, 54%
+						</p>
+					</div>
+					<div>
+						<div
+							style={{
+								padding: '3rem 1rem 1rem',
+								borderRadius: '15px',
+							}}
+							className="bg-grey-blue text-porcelain"
+						>
+							#FDA214
+						</div>
+						<p>
+							<span className="text-jungle-mist">RGB</span> 253, 162, 20
+						</p>
+						<p>
+							<span className="text-jungle-mist">HSL</span> 37, 98%, 54%
+						</p>
+					</div>
+					<div>
+						<div
+							style={{
+								padding: '3rem 1rem 1rem',
+								borderRadius: '15px',
+							}}
+							className="bg-hippie-blue text-soapstone"
+						>
+							#FDA214
+						</div>
+						<p>
+							<span className="text-jungle-mist">RGB</span> 253, 162, 20
+						</p>
+						<p>
+							<span className="text-jungle-mist">HSL</span> 37, 98%, 54%
+						</p>
+					</div>
+					<div>
+						<div
+							style={{
+								padding: '3rem 1rem 1rem',
+								borderRadius: '15px',
+							}}
+							className="bg-soapstone text-pickled-bluewood"
+						>
+							#FDA214
+						</div>
+						<p>
+							<span className="text-jungle-mist">RGB</span> 253, 162, 20
+						</p>
+						<p>
+							<span className="text-jungle-mist">HSL</span> 37, 98%, 54%
+						</p>
+					</div>
+				</div>
+			</section>
 		</div>
 	);
 };

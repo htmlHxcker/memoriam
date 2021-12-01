97e0b469e51e94e4850a0668154c4b5c6659072f
htmlHxcker <clintonadeleke@gmail.com>
2021-12-01 10:53:20 +0000

    [feat]: Create Modal Component

---
+++ src/components/Modal.js
@@ -0,0 +1,13 @@
+import './Modal.css';
+
+const Modal = () => {
+	return (
+		<div>
+			<div className="container--modal">
+				<div className="modal flow bg-soapstone"></div>
+			</div>
+		</div>
+	);
+};
+
+export default Modal;

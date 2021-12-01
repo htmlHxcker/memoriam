ad04799957c5ac273512b82f4ba3f948f04da7e0
htmlHxcker <clintonadeleke@gmail.com>
2021-12-01 11:43:03 +0000

    [feat]: Create Buttons Components

---
+++ src/components/Buttons.js
@@ -0,0 +1,25 @@
+import '../styles/utilities.css';
+
+const Button = ({ value }) => {
+	return <input type="button" value={value} className="btn" />;
+};
+
+export const MenuButton = ({ value }) => {
+	return <input type="button" value={value} className="btn menu-btn fs-600" />;
+};
+
+export const MenuSelection = ({ value }) => {
+	return <input type="button" value={value} className="btn menu-selection" />;
+};
+
+export const PrimaryButton = ({ value }) => {
+	return (
+		<input type="button" value={value} className="btn menu-btn btn-primary" />
+	);
+};
+
+export const SecondaryButton = ({ value }) => {
+	return <input type="button" value={value} className="btn btn-secondary" />;
+};
+
+export default Button;
